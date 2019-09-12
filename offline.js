238bfda583f840fb6e8ac80d91f9404af0eacc45
kgklfghfgfghtyj <52027016+kgklfghfgfghtyj@users.noreply.github.com>
2019-09-12 15:16:21 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1568276036,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite3-sheet0.png",
+		"images/dino-sheet0.png",
+		"images/dino-sheet1.png",
+		"images/dino-sheet2.png",
+		"images/sprite12-sheet0.png",
+		"images/hp-sheet0.png",
+		"images/sprite14-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
